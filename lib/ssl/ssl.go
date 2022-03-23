0094c0211e0d0340168f997f69186f6d55982854
ryuichi1208 <ryucrosskey@gmail.com>
2022-03-23 23:23:41 +0900

    first commit

--- lib/ssl/ssl.go
+++ lib/ssl/ssl.go
@@ -1,9 +1,14 @@
 package ssl
 
 import (
+	"context"
 	"crypto/tls"
 	"flag"
-	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httptrace"
+	"net/url"
 	"strings"
 	"time"
 
@@ -11,69 +16,161 @@ import (
 )
 
 type SSLPlugin struct {
-	ip         string
-	port       int
-	serverName string
-	Prefix     string
+	Prefix string
+	Url    *url.URL
 }
 
-type SSLHandShakeResult struct {
-	duration   float64
-	statusCode int
+type Result struct {
+	dnsLookupTime    float64
+	tcphandshakeTime float64
+	sslhandshakeTime float64
 }
 
 func (s SSLPlugin) GraphDefinition() map[string]mp.Graphs {
 	labelPrefix := strings.Title(s.MetricKeyPrefix())
 	return map[string]mp.Graphs{
-		"": {
+		"dnsLookupTime": {
 			Label: labelPrefix,
 			Unit:  mp.UnitFloat,
 			Metrics: []mp.Metrics{
-				{Name: "seconds", Label: "Seconds"},
+				{Name: "dnsLookupTime", Label: "dnsLookupTime"},
+			},
+		},
+		"tcphandshakeTime": {
+			Label: labelPrefix,
+			Unit:  mp.UnitFloat,
+			Metrics: []mp.Metrics{
+				{Name: "tcphandshakeTime", Label: "tcphandshakeTime"},
+			},
+		},
+		"sslhandshakeTime": {
+			Label: labelPrefix,
+			Unit:  mp.UnitFloat,
+			Metrics: []mp.Metrics{
+				{Name: "sslhandshakeTime", Label: "sslhandshakeTime"},
 			},
 		},
 	}
 }
 
-func (s SSLPlugin) DoSSLHandshake() (SSLHandShakeResult, error) {
-	ret := SSLHandShakeResult{}
-	cfg := &tls.Config{
-		ServerName: s.serverName,
-	}
-	now := time.Now()
-	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", s.ip, s.port), cfg)
-	ret.duration = float64(time.Since(now).Milliseconds())
-	if err != nil {
-		fmt.Println(err)
-		return ret, err
-	}
-	defer conn.Close()
-	return ret, nil
-}
-
-// SSLハンドシェイクをしてその時間を計測した値を返す関数
-func (s SSLPlugin) FetchMetrics() (map[string]float64, error) {
-	v, err := s.DoSSLHandshake()
-	if err != nil {
-		return nil, err
-	}
-	return map[string]float64{"seconds": v.duration}, nil
-}
-
 func (s SSLPlugin) MetricKeyPrefix() string {
-	return "SSL"
+	if s.Prefix == "" {
+		s.Prefix = "ssl"
+	}
+	return s.Prefix
+}
+
+func parseURL(uri string) *url.URL {
+	if !strings.Contains(uri, "://") && !strings.HasPrefix(uri, "//") {
+		uri = "//" + uri
+	}
+
+	url, err := url.Parse(uri)
+	if err != nil {
+		log.Fatalf("could not parse url %q: %v", uri, err)
+	}
+
+	if url.Scheme == "" {
+		url.Scheme = "https"
+	}
+	return url
+}
+
+func (res *Result) visit(url *url.URL) error {
+	req := newRequest(url)
+	var t0, t1, t5 time.Time
+
+	trace := &httptrace.ClientTrace{
+		DNSStart: func(_ httptrace.DNSStartInfo) {
+			t0 = time.Now()
+		},
+		DNSDone: func(_ httptrace.DNSDoneInfo) {
+			res.dnsLookupTime = float64(time.Since(t0).Milliseconds())
+		},
+		ConnectStart: func(_, _ string) {
+			if t1.IsZero() {
+				t1 = time.Now()
+			}
+		},
+		ConnectDone: func(net, addr string, err error) {
+			if err != nil {
+				log.Fatalf("unable to connect to host %v: %v", addr, err)
+			}
+			res.tcphandshakeTime = float64(time.Since(t1).Milliseconds())
+		},
+		TLSHandshakeStart: func() {
+			t5 = time.Now()
+		},
+		TLSHandshakeDone: func(_ tls.ConnectionState, _ error) {
+			res.sslhandshakeTime = float64(time.Since(t5).Milliseconds())
+		},
+	}
+	req = req.WithContext(httptrace.WithClientTrace(context.Background(), trace))
+
+	tr := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		ForceAttemptHTTP2:     true,
+	}
+
+	host, _, err := net.SplitHostPort(req.Host)
+	if err != nil {
+		host = req.Host
+	}
+
+	tr.TLSClientConfig = &tls.Config{
+		ServerName:         host,
+		InsecureSkipVerify: false,
+		MinVersion:         tls.VersionTLS12,
+	}
+
+	client := &http.Client{
+		Transport: tr,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	return nil
+}
+
+func newRequest(url *url.URL) *http.Request {
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		log.Fatalf("unable to create request: %v", err)
+	}
+	return req
+}
+
+func (s SSLPlugin) FetchMetrics() (map[string]float64, error) {
+	res := Result{}
+	err := res.visit(s.Url)
+	if err != nil {
+		return make(map[string]float64), nil
+	}
+	return map[string]float64{
+		"dnsLookupTime":    res.dnsLookupTime,
+		"tcphandshakeTime": res.tcphandshakeTime,
+		"sslhandshakeTime": res.sslhandshakeTime,
+	}, nil
 }
 
 func Do() {
-	ip := flag.String("ip", "", "description")
-	serverName := flag.String("servername", "", "description")
-	port := flag.Int("port", 443, "description")
+	optPrefix := flag.String("prefix", "", "Metric key prefix")
 	flag.Parse()
+	args := flag.Args()
+	url := parseURL(args[0])
 	s := SSLPlugin{
-		Prefix:     "SSL",
-		ip:         *ip,
-		serverName: *serverName,
-		port:       *port,
+		Prefix: *optPrefix,
+		Url:    url,
 	}
 	plugin := mp.NewMackerelPlugin(s)
 	plugin.Run()
